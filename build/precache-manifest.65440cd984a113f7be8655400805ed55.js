062eebbf71c9b2564c2c89ecb3ac93f088568400
TalishGargg <67477834+TalishGargg@users.noreply.github.com>
2020-09-18 12:17:32 +0400

    Add files via upload

---
+++ build/precache-manifest.65440cd984a113f7be8655400805ed55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd9693f8c964819a88696ec56710b27c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a67f6c3e81647314b06",
+    "url": "/static/css/main.c9c71959.chunk.css"
+  },
+  {
+    "revision": "757e0f24468dbb15b2b3",
+    "url": "/static/js/2.74e6b852.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.74e6b852.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a67f6c3e81647314b06",
+    "url": "/static/js/main.23dccf59.chunk.js"
+  },
+  {
+    "revision": "a9bcb2082ea44ffc1df4",
+    "url": "/static/js/runtime-main.ec8f8518.js"
+  }
+]);
